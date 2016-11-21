55dc4f2a0e084db9bc4161a68d12f3c5c7859902
b.h.kim <kbh9125@naver.com>
2016-11-21 15:20:24 +0900

    Profile Class Create

---
+++ src/godofjava/Profile.java
@@ -0,0 +1,8 @@
+package godofjava;
+
+public class Profile {
+	public static void main(String[] args) {
+		System.out.println("My name is Kim Bang Hyun");
+		System.out.println("My ages is 18");
+	}
+}
