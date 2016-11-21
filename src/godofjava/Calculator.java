9fef259b8409efd5f53a2e6bc61e2b1b39850582
b.h.kim <kbh9125@naver.com>
2016-11-21 15:13:14 +0900

    Calculator class Create

---
+++ src/godofjava/Calculator.java
@@ -0,0 +1,24 @@
+package godofjava;
+
+public class Calculator {
+	public int add(int num1, int num2) {
+		int sum;
+		sum = num1 + num2;
+		return sum;
+	}
+	public int subtract(int num1, int num2) {
+		int sum;
+		sum = num1 - num2;
+		return sum;
+	}
+	public int multiply(int num1, int num2) {
+		int sum;
+		sum = num1 * num2;
+		return sum;
+	}
+	public int divide(int num1, int num2) {
+		int sum;
+		sum = num1 / num2;
+		return sum;
+	}
+}
