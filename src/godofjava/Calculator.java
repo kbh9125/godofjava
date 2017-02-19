9f3b569dcf05bba3321a113682c03d7fc1ca20ed
b.h.kim <kbh9125@naver.com>
2017-02-19 22:55:29 +0900

    main 메소드 구현

--- src/godofjava/Calculator.java
+++ src/godofjava/Calculator.java
@@ -1,6 +1,16 @@
 package godofjava;
 
 public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("Calculator class started");
+		Calculator calc = new Calculator();
+		int a = 10;
+		int b = 5;
+		System.out.println("add="+calc.add(a, b));
+		System.out.println("substract="+calc.subtract(a, b));
+		System.out.println("multiply="+calc.multiply(a, b));
+		System.out.println("divide="+calc.divide(a, b));
+	}
 	public int add(int num1, int num2) {
 		int sum;
 		sum = num1 + num2;
