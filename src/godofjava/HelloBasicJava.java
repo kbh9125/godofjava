2052884c6e5795e1649ae3af3524cfe6232edc8f
b.h.kim <kbh9125@naver.com>
2017-02-19 21:46:02 +0900

    add file

---
+++ src/godofjava/HelloBasicJava.java
@@ -0,0 +1,8 @@
+package godofjava;
+
+public class HelloBasicJava {
+	public static void main(String[] args) {
+		System.out.println("Hello God of Java!!!!");
+		System.out.println("I love this book");
+	}
+}
