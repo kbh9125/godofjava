7fdb8298359a31186fca4d256c2c989457c755c1
b.h.kim <kbh9125@naver.com>
2017-03-05 23:57:11 +0900

    add file

---
+++ src/godofjava/inheritance/Child.java
@@ -0,0 +1,18 @@
+package godofjava.inheritance;
+
+public class Child extends Parent {
+	public Child() {
+		// Parent 클래스의 생성자중 null을 매개 변수로 받을 수 있는 생성자를 찾는다.
+		super(null);
+		System.out.println("Child Construnctor");
+	}
+	public Child(String name) {
+		// super() 는 반드시 생성자의 첫줄에 있어야 한다.
+		super(name);
+		System.out.println("Child(String) Constructor");
+	}
+	public void printName() {
+		System.out.println("printName() - Child");
+	}
+
+}
